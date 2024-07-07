a5f78d6bd09bbcc32d3c3399cefbe2829ffd441c
lenovo <ritiraj2909@gmail.com>
2024-07-07 19:04:23 -0400

    login xml changes done

--- app/src/main/java/com/example/healtcareapp/Registration.java
+++ app/src/main/java/com/example/healtcareapp/Registration.java
@@ -1,8 +1,6 @@
 package com.example.healtcareapp;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+public class Registration extends AppCompatActivity {
 
-public class Login extends AppCompatActivity {
-    EditText edusername, edpassword;
-    Button btn
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
