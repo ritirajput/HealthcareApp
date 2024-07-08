0d6caa86e888d792ee984bb1d86af523729b13dc
lenovo <ritiraj2909@gmail.com>
2024-07-08 19:54:55 -0400

    background add in login xml

--- app/src/main/java/com/example/healtcareapp/Login.java
+++ app/src/main/java/com/example/healtcareapp/Login.java
@@ -1,8 +1,11 @@
 package com.example.healtcareapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
     EditText edusername, edpassword;
-    Button btn
+    Button btn;
+    TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        edusername = findViewById(R.id.email);
+        edpassword = findViewById(R.id.password);
+        btn = findViewById(R.id.loginbtn);
+        txt = findViewById(R.id.register);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
