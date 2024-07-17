df9da9b9b606146cf1da513e6865ba891b217842
lenovo <ritiraj2909@gmail.com>
2024-07-16 20:12:41 -0400

    code to store data in database

--- app/src/main/java/com/example/healtcareapp/Database.java
+++ app/src/main/java/com/example/healtcareapp/Database.java
@@ -2,34 +2,49 @@ package com.example.healtcareapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class Database extends SQLiteOpenHelper {
-        public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
- String qry1 = "create table users( username txt, email txt, password txt )";
- db.execSQL(qry1);
+        String qry1 = "create table users( username txt, email txt, password txt )";
+        db.execSQL(qry1);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public void register(String username, String email, String password, String conpassword){
+
+    public void register(String username, String email, String password, String conpassword) {
         ContentValues cv = new ContentValues();
         cv.put("Username", username);
         cv.put("Email", email);
         cv.put("Password", password);
         cv.put("Confirm Password", conpassword);
         SQLiteDatabase db = getWritableDatabase();
-        db.insert("users" , null,cv);
+        db.insert("users", null, cv);
         db.close();
+    }
+
+    public int login(String username, String password) {
+        int result = 0;
+        String str[] = new String[2];
+        str[0] = username;
+        str[1] = password;
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery("select * from users where username =? and password =?", str);
+        if (c.moveToFirst()) {
+            result = 1;
+        }
+        return result;
     }
 }
--- app/src/main/java/com/example/healtcareapp/Login.java
+++ app/src/main/java/com/example/healtcareapp/Login.java
@@ -1,6 +1,8 @@
 package com.example.healtcareapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,10 +38,20 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 String username = edusername.getText().toString();
                 String password = edpassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"healtYOU",null, 1);
                 if (username.length() == 0 || password.length() == 0) {
                     Toast.makeText(getApplicationContext(), "Complete the details or register yourself", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    if(db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString("username",username);
+                        editor.apply();
+                        startActivity(new Intent(Login.this,Home.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Invalid username or password" , Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
